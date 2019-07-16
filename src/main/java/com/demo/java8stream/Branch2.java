18c23fc8075de2b9f88b30f283932ab45d37d6f1
XiongChuanHong <18881934641@163.com>
2019-07-16 08:56:21 +0800

    2

--- src/main/java/com/demo/java8stream/Branch2.java
+++ src/main/java/com/demo/java8stream/Branch2.java
@@ -9,6 +9,7 @@ public class Branch2 {
     public static void main(String[] args) {
 
         System.out.println("ha ha ha ha ha");
+        System.out.println("ha ha ha ha ha");
         System.out.println("branch2 is not finished");
     }
 }
