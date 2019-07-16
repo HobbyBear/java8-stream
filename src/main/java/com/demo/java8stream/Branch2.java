ecf8f1306527ab2bd15b52041f5b32e578feeb3b
XiongChuanHong <18881934641@163.com>
2019-07-16 08:57:15 +0800

    4

--- src/main/java/com/demo/java8stream/Branch2.java
+++ src/main/java/com/demo/java8stream/Branch2.java
@@ -11,6 +11,7 @@ public class Branch2 {
         System.out.println("ha ha ha ha ha");
         System.out.println("ha ha ha ha ha");
         System.out.println("无敌");
+        System.out.println("activity");
         System.out.println("branch2 is not finished");
     }
 }
