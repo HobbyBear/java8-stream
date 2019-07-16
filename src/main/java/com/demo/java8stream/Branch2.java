9dd82c746beb77f6589be36da9fc1cf5d77f1616
XiongChuanHong <18881934641@163.com>
2019-07-16 08:39:04 +0800

    branch1 init

---
+++ src/main/java/com/demo/java8stream/Branch2.java
@@ -0,0 +1,12 @@
+package com.demo.java8stream;
+
+/**
+ * @author: xch
+ * @create: 2019-07-16 08:35
+ **/
+public class Branch2 {
+
+    public static void main(String[] args) {
+        System.out.println("branch2 is not finished");
+    }
+}
