e621fbdc6682c34eb0e0bea58309502fdd949e65
XiongChuanHong <18881934641@163.com>
2019-07-16 08:32:25 +0800

    branch1 init

---
+++ src/main/java/com/demo/java8stream/Branch1.java
@@ -0,0 +1,12 @@
+package com.demo.java8stream;
+
+/**
+ * @author: xch
+ * @create: 2019-07-16 08:31
+ **/
+public class Branch1 {
+
+    public static void main(String[] args) {
+        System.out.println("this is branch1");
+    }
+}
